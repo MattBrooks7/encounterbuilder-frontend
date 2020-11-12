a546a23701abe51692069e06401c54bc58bddc52
Matt Brooks <mattbrooks2010@gmail.com>
2020-11-12 15:34:29 -0600

    brain mush

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { MonstersComponent } from './components/monsters/monsters.component';
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -14,6 +15,10 @@ const routes: Routes = [
   {
     path: 'encounter',
     component: EncounterComponent
+  },
+  {
+    path: 'monsters',
+    component: MonstersComponent
   }
 ];
 
